8c47cf3c869f37b12c88f411a0ad6b5c5f5d6675
Billy <510811366@qq.com>
2016-02-01 09:50:38 +0800

    react-native-api-demo
    根据API 自己写的一些DEMO（持续更新中。。。欢迎收藏加星）

---
+++ index.android.js
@@ -0,0 +1,30 @@
+
+'use strict';
+
+var React = require('react-native');
+//var TabBarView = require('./Application/View/TabBar');
+
+/*布局 Test*/
+var LayoutView = require('./Application/Test/Layout');
+
+/*API Test*/
+var ActionSheelIOS = require('./Application/Test/ActionSheelIOS');
+var Alert = require('./Application/Test/Alert');
+var AlertIOS = require('./Application/Test/AlertIOS');
+var Animated = require('./Application/Test/Animated');
+
+var {
+    AppRegistry,
+} = React;
+
+var NV = React.createClass({
+
+    render : function() {
+        return (
+            <Animated />
+        );
+    }
+
+});
+
+AppRegistry.registerComponent('test', () => NV);
